c2b58b9972f8904bb80c4a7c68f59acc6f563dfd
bhatnags <bhatnags@tcd.ie>
2020-01-30 22:42:55 +0000

    added job apply code

---
+++ jobs_assignment/rev/revolut_test_docker/try.py
@@ -0,0 +1,23 @@
+
+# =============================================================================
+# a)	Define a target metric to measure user engagement. How would you define an engaged vs. unengaged user? 
+# 
+# b)	Using your logic from above, build a model (heuristic/statistical/ML) 
+#     to classify engaged and unengaged users. 
+#     Note that features which are directly correlated with your target metric could lead to overfitting.
+# 
+# c)	Let’s assume an unengaged user is a churned user. 
+#     Now suppose we use your model to identify unengaged users 
+#     and implement some business actions try to convert them to engaged users 
+#     (commonly known as reducing churn). 
+#     How would you set up a test/experiment to check whether we are actually reducing churn?
+# 
+# d)	In the past, one business action we took to reduce churn was to re-engage inactive users. 
+#     Our engagement team designed a marketing campaign, 
+#     REENGAGEMENT_ACTIVE_FUNDS to remind inactive users about funds on their Revolut account. 
+#     Define a metric to measure the effectiveness of the campaign. 
+#     Under this metric, was the campaign effective?
+# =============================================================================
+
+
+
